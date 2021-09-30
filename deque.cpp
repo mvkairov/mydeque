a0c43136b95df0d718c923a2524ba827962b94ea
Matvey Kairov <kairovmatvey@gmail.com>
2021-09-30 22:26:11 +0300

    qq

---
+++ deque.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matvey Kairov on 30.09.2021.
+//
+
+#include "deque.h"
---
+++ deque.h
@@ -0,0 +1,16 @@
+//
+// Created by Matvey Kairov on 30.09.2021.
+//
+
+#ifndef MYDEQUE_DEQUE_H
+#define MYDEQUE_DEQUE_H
+
+
+
+class deque {
+
+};
+
+
+
+#endif //MYDEQUE_DEQUE_H
